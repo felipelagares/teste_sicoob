9e185097e68c7fab065b288a4862d762f5372b48
felipelagares <62161177+felipelagares@users.noreply.github.com>
2026-07-28 14:35:18 -0300

    Update drive.py

--- google_api/drive.py
+++ google_api/drive.py
@@ -1,48 +1,70 @@
-from google.oauth2.service_account import Credentials
+from pathlib import Path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-SERVICE_ACCOUNT_FILE = "config/credentials.json"
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
 
 def get_drive_service():
-    credentials = Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
-        scopes=SCOPES
-    )
+    creds = None
 
-    return build("drive", "v3", credentials=credentials)
+    token = Path("config/token.json")
 
+    if token.exists():
+        creds = Credentials.from_authorized_user_file(
+            token,
+            SCOPES
+        )
+
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "config/client_secret.json",
+                SCOPES
+            )
+
+            creds = flow.run_local_server(port=0)
+
+        token.write_text(creds.to_json())
+
+    return build("drive", "v3", credentials=creds)
 
 def create_folder(folder_name):
     service = get_drive_service()
 
-    folder_metadata = {
+    metadata = {
         "name": folder_name,
         "mimeType": "application/vnd.google-apps.folder"
     }
 
     folder = service.files().create(
-        body=folder_metadata,
+        body=metadata,
         fields="id"
     ).execute()
 
     return folder["id"]
 
-
 def upload_image(file_path, folder_id):
+
     service = get_drive_service()
 
-    file_metadata = {
-        "name": file_path.split("/")[-1],
+    metadata = {
+        "name": Path(file_path).name,
         "parents": [folder_id]
     }
 
     media = MediaFileUpload(file_path)
 
     file = service.files().create(
-        body=file_metadata,
+        body=metadata,
         media_body=media,
         fields="id, webViewLink"
     ).execute()
