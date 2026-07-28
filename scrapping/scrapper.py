52f9649040ee71ac6c21176a943583c57e0fc0bc
felipelagares <62161177+felipelagares@users.noreply.github.com>
2026-07-28 13:58:29 -0300

    Update scrapper.py

--- scrapping/scrapper.py
+++ scrapping/scrapper.py
@@ -1,44 +1,38 @@
 import selenium
-
-from logger import default_logger
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-from image_downloader import download_image
-
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install())
-)
+from logs.logger import default_logger
 
 
-#chromedriver_path ="chromedriver\chromedriver.exe"
+def page_scraper(driver: selenium.webdriver.Chrome):
+    books = []
+    for index in range(1, 21):
+        book_xpath = f'//*[@id="default"]/div/div/div/div/section/div[2]/ol/li[{index}]'
 
-#driver = selenium.webdriver.Chrome(executable_path=chromedriver_path)
+        image = "/article/div[1]/a/img"
+        book_cover = driver.find_element(
+            "xpath", book_xpath + image).get_attribute("src")
 
-driver.get("https://books.toscrape.com/catalogue/page-1.html")
+        name_path = "/article/h3/a"
+        book_name = driver.find_element("xpath", book_xpath + name_path).text
 
-for index in range(1, 21):
-    book_xpath = f'//*[@id="default"]/div/div/div/div/section/div[2]/ol/li[{index}]'
+        price_path = "/article/div[2]/p[1]"
+        book_price = driver.find_element("xpath", book_xpath + price_path).text
 
-    image = "/article/div[1]/a/img"
-    book_cover = driver.find_element(
-        "xpath", book_xpath + image).get_attribute("src")
+        rating_path = "/article/p"
+        book_rating = driver.find_element("xpath", book_xpath + rating_path).get_attribute("class")
+        book_rating = book_rating.replace("star-rating ", "")
 
-    name_path = "/article/h3/a"
-    book_name = driver.find_element("xpath", book_xpath + name_path).text
+        availability_path = "/article/div[2]/p[2]"
+        book_availability = driver.find_element(
+            "xpath", book_xpath + availability_path).text
 
-    price_path = "/article/div[2]/p[1]"
-    book_price = driver.find_element("xpath", book_xpath + price_path).text
+        default_logger.info(f"\nindex: {index}\n Book Name: {book_name}\n Book Price: {book_price}\n Book Rating: {book_rating}\n Book Availability: {book_availability}\n")
 
-    rating_path = "/article/p"
-    book_rating = driver.find_element("xpath", book_xpath + rating_path).get_attribute("class")
-    book_rating = book_rating.replace("star-rating ", "")
+        books.append({
+            "name": book_name,
+            "cover": book_cover,
+            "price": book_price,
+            "rating": book_rating,
+            "availability": book_availability
+        })
 
-    availability_path = "/article/div[2]/p[2]"
-    book_availability = driver.find_element(
-        "xpath", book_xpath + availability_path).text
-
-    default_logger.info(f"\nindex: {index}\n Book Name: {book_name}\n Book Price: {book_price}\n Book Rating: {book_rating}\n Book Availability: {book_availability}\n")
-
-    download_image(book_cover, book_name)
+    return books
